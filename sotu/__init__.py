a1fb7665a14842a103c9093d3b73ead2f55d1c51
Kyle Fuller <kyle@fuller.li>
2015-06-02 13:06:54 -0700

    Email exceptions to CocoaPods team

--- sotu/__init__.py
+++ sotu/__init__.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 from rivr.wsgi import WSGIHandler
@@ -8,6 +9,25 @@ logger = logging.getLogger('rivr.request')
 console = logging.StreamHandler()
 console.setLevel(logging.ERROR)
 logger.addHandler(console)
+
+if os.environ.get('SENDGRID_USERNAME'):
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('smtp.sendgrid.net',
+                               'info@cocoapods.org',
+                               ['info@cocoapods.org'],
+                               'State of the Union Heroku Exception',
+                               credentials=(os.environ['SENDGRID_USERNAME'],
+                                            os.environ['SENDGRID_PASSWORD']))
+    mail_handler.setLevel(logging.ERROR)
+    logger.addHandler(mail_handler)
+    mail_handler.setFormatter(logging.Formatter("""\
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+Message:
+%(message)s"""))
 
 wsgi = WSGIHandler(middleware)
 
