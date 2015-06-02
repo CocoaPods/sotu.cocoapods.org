609a2f2d8605181bae852274e5e5a6d1aa8c20e4
Kyle Fuller <kyle@fuller.li>
2015-06-02 13:02:11 -0700

    [logging] Log errors to sterr

--- sotu/__init__.py
+++ sotu/__init__.py
@@ -1,5 +1,13 @@
+import logging
+
 from rivr.wsgi import WSGIHandler
 from sotu.middleware import middleware
+
+
+logger = logging.getLogger('rivr.request')
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+logger.addHandler(console)
 
 wsgi = WSGIHandler(middleware)
 
